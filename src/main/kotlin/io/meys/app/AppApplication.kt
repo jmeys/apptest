fe73ee7f7c83c69ea5105de1e939c1ede170ec38
jmeys <jeroen.meys@ordina.be>
2019-12-29 23:58:34 +0100

    first commit

---
+++ src/main/kotlin/io/meys/app/AppApplication.kt
@@ -0,0 +1,20 @@
+package io.meys.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class AppApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppApplication>(*args)
+}
+
+@RestController
+class RootController {
+	
+	@GetMapping
+	fun foo() = "Hello, World!"
+}
